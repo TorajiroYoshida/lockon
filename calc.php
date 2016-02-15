9f59fe579c1cb4d1f512466d8fb5ac44dd3896aa
Tomoyuki Sueda <fkenf339@yahoo.co.jp>
2016-02-15 11:54:09 +0900

    calculation

---
+++ calc.php
@@ -0,0 +1,61 @@
+<?php
+$num1 = filter_input(INPUT_POST, 'num1');
+$operator = filter_input(INPUT_POST, 'operator');
+$num2 = filter_input(INPUT_POST, 'num2');
+
+if (!is_null($num1) && !is_null($num2)) {
+    switch ($operator) {
+        case '-':
+            $answer = $num1 - $num2;
+            break;
+        case '*':
+            $answer = $num1 * $num2;
+            break;
+        case '/':
+            $answer = $num1 / $num2;
+            break;
+        case '+':
+            $answer = $num1 + $num2;
+            break;
+        default:
+    }
+    $result = "{$num1} {$operator} {$num2} = {$answer}";
+} else {
+    $result = '計算結果なし';
+}
+?>
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+ and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>電卓の基本</title>
+    </head>
+    <body>
+        <form method = "POST">
+            <input type="text" name="num1" value="<?php echo $num1; ?>" required autofocus/>
+            <select name="operator">
+                <option value="+" <?php if ($operator === '+') {
+    echo 'selected';
+} ?>>+</option>
+                <option value="-" <?php if ($operator === '-') {
+    echo 'selected';
+} ?>>-</option>
+                <option value="*" <?php if ($operator === '*') {
+    echo 'selected';
+} ?>>*</option>
+                <option value="/" <?php if ($operator === '/') {
+    echo 'selected';
+} ?>>/</option>
+            </select>
+            <input type="text" name="num2" value="<?php echo $num2; ?>" required/>
+            <input type="submit" value="計算する">
+        </form>
+        <p><?php echo $result; ?></p>
+    </body>
+</html>
